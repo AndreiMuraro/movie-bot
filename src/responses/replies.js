66159468abe179ea7554ce4c077ba3cb834884b0
Andrei Muraro <muraroandrei@gmail.com>
2025-04-18 16:03:17 -0300

    leve mudanças nada sério

---
+++ src/responses/replies.js
@@ -0,0 +1,21 @@
+// responses.js
+export const replies = {
+    Deus: [
+        { reply: 'Deus*', weight: 0.8 },
+        { reply: 'Deus* é maiusculo, minúsculo é você perto dele!', weight: 0.1 },
+        { reply: 'Deus* seu herege!', weight: 0.1 },
+    ],
+    Roger: [
+        { reply: 'Bééh 🐑🧎‍♂️', weight: 1 },
+        { reply: 'Bééhelp 🐑🧎‍♂️', weight: 1 },
+        { reply: 'Bééh bééh bééh 🐑🧎‍♂️', weight: 1 },
+        { reply: 'au au au 🐕🧎‍♂️', weight: 1 },
+        { reply: 'auAiuiAu au au 🐕🧎‍♂️', weight: 1 },
+        { reply: 'miau miau miau 🐈🧎‍♂️', weight: 1 },
+        { reply: 'miaudeus miau quedelicia 🐈🧎‍♂️', weight: 1 },
+        { reply: 'muuuuh 🐄🧎‍♂️', weight: 1 },
+        { reply: 'muuuuh muuuuh muuuuh 🐄🧎‍♂️', weight: 1 },
+        { reply: 'muuuuhito bom roger 🐄🧎‍♂️', weight: 1 },
+        { reply: 'https://www.youtube.com/watch?v=J_u3T4A-2Ko', weight: 1 },
+    ]
+};
